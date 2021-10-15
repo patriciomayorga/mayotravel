6d4e8eb36a864ab0bb5528ec1ec22c68c67008c5
Patricio Mayorga <patricio11mayorga@hotmail.com>
2021-10-15 17:22:21 -0500

    cambios realizados head

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import Head from 'next/head';
+
 import './app.css';
 import '../styles/styles.css';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Mayotravel</title>
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
       Footer
     </>
