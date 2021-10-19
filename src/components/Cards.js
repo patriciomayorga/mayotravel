ac94bab5aa3ac44ad0b3571d7a178803c3511cd4
Patricio Mayorga <patricio11mayorga@hotmail.com>
2021-10-18 20:31:14 -0500

    cambios listos

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,35 +1,71 @@
-import React from 'react'
+import React from 'react';
 import Container from './Container';
-import {EnvironmentOutlined} from '@ant-design/icons';
-import { Row, Col } from 'antd';
-
+import { EnvironmentOutlined, InfoCircleOutlined } from '@ant-design/icons';
+import { Row, Col, Tag, Popover } from 'antd';
 
 export const Cards = () => {
-    return (
-        <Container>
-            <div className="place-tittle">
-            <EnvironmentOutlined />
-                Disfruta Baños de Agua Santa
+  const content = (
+    <div className='card-content-popover-content'>
+      Si tus planes cambian, puedes cancelar la actividad.
+    </div>
+  );
+  return (
+    <Container>
+      <div className='place-tittle'>
+        <EnvironmentOutlined /> Disfruta Baños de Agua Santa
+      </div>
+      <div className='container-cards'>
+        <Row gutter={15}>
+          <Col span={6}>
+            <div className='card'>
+              <img
+                className='photo'
+                src='https://media.staticontent.com/media/pictures/5192c06f-10ab-44e8-bfe2-9c03913fbebb/295x200'
+              />
+              <div className='card-content'>
+                <h4 className='card-content-subtitle'>Actividades</h4>
+                <h3 className='card-content-title'>Rafting</h3>
+                <Popover className='card-content-popover' placement='topLeft' content={content}>
+                  <Tag color='#f50'>
+                    <InfoCircleOutlined /> Reserva Flexible
+                  </Tag>
+                </Popover>
+              </div>
+              <div className='card-footer'>
+                Precio por adulto desde:
+                <div className='card-footer-inside'>
+                  <h4 className='card-footer-usd'>Usd</h4>
+                  <h3 className='card-footer-number'>40</h3>
+                </div>
+              </div>
             </div>
-            <div className="container-cards">
-            <div className="cards">
-                <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
-                <Col className="gutter-row" span={6}>
-                    <h1>aqui van los cards</h1>
-                </Col>
-                <Col className="gutter-row" span={6}>
-                <h1>aqui van los cards</h1>
-                </Col>
-                <Col className="gutter-row" span={6}>
-                <h1>aqui van los cards</h1>
-                </Col>
-                <Col className="gutter-row" span={6}>
-                <h1>aqui van los cards</h1>
-                </Col>
-                </Row>     
+          </Col>
+          <Col span={6}>
+            <div className='card'>
+              <img
+                className='photo'
+                src='https://media.staticontent.com/media/pictures/5192c06f-10ab-44e8-bfe2-9c03913fbebb/295x200'
+              />
             </div>
-        </div>
-        </Container>
-        
-    )
-}
+          </Col>
+          <Col span={6}>
+            <div className='card'>
+              <img
+                className='photo'
+                src='https://media.staticontent.com/media/pictures/5192c06f-10ab-44e8-bfe2-9c03913fbebb/295x200'
+              />
+            </div>
+          </Col>
+          <Col span={6}>
+            <div className='card'>
+              <img
+                className='photo'
+                src='https://media.staticontent.com/media/pictures/5192c06f-10ab-44e8-bfe2-9c03913fbebb/295x200'
+              />
+            </div>
+          </Col>
+        </Row>
+      </div>
+    </Container>
+  );
+};
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,13 +3,12 @@ import Head from 'next/head';
 
 import './app.css';
 import '../styles/styles.css';
-import { Footer } from 'antd/lib/layout/layout';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset='UTF-8' />
+        <meta charSet='UTF-8' />
         <title>
           Mayotravel Agencia de Viajes | Baños Ecuador | Galapagos | Rafting | Canyoning | Paquetes
           | Tours | Hoteles | Vuelos Charter | Vacaciones
@@ -23,7 +22,7 @@ function MyApp({ Component, pageProps }) {
           name='keywords'
           content='Baños Ecuador, Tours a Galapagos, Rafting en Baños Ecuador, Canyoning en Baños Ecuador, Tours, Hoteles, Vuelos Charter, Paquetes, Decameron Mompiche, Tours a Panama, Tours a Cartagena, Tours a Curacao, Tours a Jamaica, Tours a Mexico, Tours a Punta Cana, Tours a San Andres, Cuyabeno Ecuador, Quilotoa Ecuador, Chimborazo Ecuador, Cotopaxi Ecuador, Lodges Ecuador, Mayotravel Agencia de Viajes, Agencias de Viajes Ecuador, Agencias de Viajes Baños, Biking, Tours a Baños'
         />
-        <meta name='robots' content='index, follow' />
+        <meta name='robots' content='noindex, nofollow' />
         <meta property='og:site_name' content='Mayotravel Agencia de Viajes Baños Ecuador' />
         <meta
           property='og:title'
@@ -39,15 +38,6 @@ function MyApp({ Component, pageProps }) {
 
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
         <link rel='icon' href='/favicon.ico' type='image/x-icon' />
-
-        <meta
-          property='og:image'
-          content='https://img.goraymi.com/2018/10/01/a388b3def8c367db18c7901bb5923c3b_xl.jpg'
-        />
-        <meta
-          property='og:description'
-          content='Mayotravel es la principal Agencia de Viajes Mayorista en Ecuador, especializada en la organización de tours de aventura en Baños y todo el Ecuador, Reserva de Hoteles, Vuelos Charter, Islas Galápagos y Paquetes Internacionales con el servicio Todo Incluido.'
-        />
         <meta name='twitter:card' content='summary_large_image' />
         <meta
           name='twitter:description'
