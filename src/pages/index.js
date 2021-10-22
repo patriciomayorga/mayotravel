abb719c1f26a654d3c82136386cfe275541f8070
Patricio Mayorga <patricio11mayorga@hotmail.com>
2021-10-22 11:51:05 -0500

    master 22

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,8 @@ import { Cards } from '../components/Cards';
 import Header from '../components/Header';
 import Video from '../components/Video';
 import { Footer } from '../components/Footer';
+import { Cards2 } from '../components/Cards2';
+import Mail from '../components/Mail';
 
 export default function HomePage() {
   return (
@@ -12,6 +14,8 @@ export default function HomePage() {
       <Video />
       <Cards />
       <Baners />
+      <Cards2 />
+      <Mail />
       <Footer />
     </div>
   );
