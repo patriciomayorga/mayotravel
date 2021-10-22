95d02a926ef54847de78cbd9ca15d1e5587a2d37
Patricio Mayorga <patricio11mayorga@hotmail.com>
2021-10-22 13:12:01 -0500

    like

--- src/components/Baners.js
+++ src/components/Baners.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Container from './Container';
-import { Row, Col } from 'antd';
+import { Row, Col, Card } from 'antd';
 import {
   ShoppingCartOutlined,
   EnvironmentOutlined,
@@ -17,52 +17,58 @@ export const Baners = () => {
       <div className='baners-box'>
         <Row gutter={15}>
           <Col span={12}>
-            <div className='photo-content'>
-              <img
-                className='photo-oferts1 '
-                alt='Islas Galápagos Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634686393/mayotravel/card-galapagos-ecuador_mmyf4s.jpg'
-              />
-              <div className='promotion-discount'>Hasta 15 % off</div>
-              <div className='banner-content'>
-                <div className='banner-content-subtitle'>Ecuador</div>
-                <div className='banner-content-title'>
-                  <EnvironmentOutlined /> Islas Galápagos
+            <Card hoverable>
+              <div className='photo-content'>
+                <img
+                  className='photo-oferts1 '
+                  alt='Islas Galápagos Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634686393/mayotravel/card-galapagos-ecuador_mmyf4s.jpg'
+                />
+                <div className='promotion-discount'>Hasta 15 % off</div>
+                <div className='banner-content'>
+                  <div className='banner-content-subtitle'>Ecuador</div>
+                  <div className='banner-content-title'>
+                    <EnvironmentOutlined /> Islas Galápagos
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
           <Col span={6}>
-            <div className='photo-content2'>
-              <img
-                className='photo-oferts1 '
-                alt='Decameron Mompiche Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634686657/mayotravel/card-decameron-ecuador_qcf29e.jpg'
-              />
-              <div className='promotion-discount2'>10 % off</div>
-              <div className='banner-content2'>
-                <div className='banner-content-subtitle2'>Hoteles</div>
-                <div className='banner-content-title2'>
-                  <HomeOutlined /> Decameron Mompiche
+            <Card hoverable>
+              <div className='photo-content2'>
+                <img
+                  className='photo-oferts1 '
+                  alt='Decameron Mompiche Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634926237/mayotravel/card-decameron-ecuador_bys5e4.jpg'
+                />
+                <div className='promotion-discount2'>10 % off</div>
+                <div className='banner-content2'>
+                  <div className='banner-content-subtitle2'>Hoteles</div>
+                  <div className='banner-content-title2'>
+                    <HomeOutlined /> Decameron Mompiche
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
           <Col span={6}>
-            <div className='photo-content2'>
-              <img
-                className='photo-oferts1 '
-                alt='Climbing Volcan Cotopaxi Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634685674/mayotravel/card-climbing-ecuador_meqff7.jpg'
-              />
-              <div className='promotion-discount3'>5 % off</div>
-              <div className='banner-content2'>
-                <div className='banner-content-subtitle2'>Climbing</div>
-                <div className='banner-content-title2'>
-                  <PushpinOutlined /> Volcán Cotopaxi
+            <Card hoverable>
+              <div className='photo-content2'>
+                <img
+                  className='photo-oferts1 '
+                  alt='Climbing Volcan Cotopaxi Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634925965/mayotravel/card-climbing-ecuador_hljbfj.jpg'
+                />
+                <div className='promotion-discount3'>5 % off</div>
+                <div className='banner-content2'>
+                  <div className='banner-content-subtitle2'>Climbing</div>
+                  <div className='banner-content-title2'>
+                    <PushpinOutlined /> Volcán Cotopaxi
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
         </Row>
       </div>
--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Container from './Container';
 import { EnvironmentOutlined, InfoCircleOutlined, PushpinOutlined } from '@ant-design/icons';
-import { Row, Col, Popover } from 'antd';
+import { Row, Col, Popover, Card } from 'antd';
 
 export const Cards = () => {
   const content = (
@@ -17,112 +17,120 @@ export const Cards = () => {
       <div className='container-cards'>
         <Row gutter={15}>
           <Col span={6}>
-            <div className='card'>
-              <img
-                className='photo'
-                alt='Rafting Baños Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-rafting-banos-ecuador_dqggg9.jpg'
-              />
-              <div className='card-content'>
-                <div className='card-content-subtitle'>
-                  <PushpinOutlined /> Actividades
+            <Card hoverable>
+              <div className='card'>
+                <img
+                  className='photo'
+                  alt='Rafting Baños Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-rafting-banos-ecuador_dqggg9.jpg'
+                />
+                <div className='card-content'>
+                  <div className='card-content-subtitle'>
+                    <PushpinOutlined /> Actividades
+                  </div>
+                  <div className='card-content-title'>Rafting</div>
+                  <Popover className='card-content-popover' placement='topLeft' content={content}>
+                    <span className='reserva-flexible'>
+                      <InfoCircleOutlined /> Reserva Flexible
+                    </span>
+                  </Popover>
                 </div>
-                <div className='card-content-title'>Rafting</div>
-                <Popover className='card-content-popover' placement='topLeft' content={content}>
-                  <span className='reserva-flexible'>
-                    <InfoCircleOutlined /> Reserva Flexible
-                  </span>
-                </Popover>
-              </div>
-              <div className='card-footer'>
-                Precio por adulto desde:
-                <div className='card-footer-inside'>
-                  <div className='card-footer-usd'>Usd</div>
-                  <div className='card-footer-number'>30</div>
+                <div className='card-footer'>
+                  Precio por adulto desde:
+                  <div className='card-footer-inside'>
+                    <div className='card-footer-usd'>Usd</div>
+                    <div className='card-footer-number'>30</div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
           <Col span={6}>
-            <div className='card'>
-              <img
-                className='photo'
-                alt='Las manos de Dios Baños Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-las-manos-de-dios-banos-ecuador_pgbows.jpg'
-              />
-              <div className='card-content'>
-                <div className='card-content-subtitle'>
-                  <PushpinOutlined /> Actividades
+            <Card hoverable>
+              <div className='card'>
+                <img
+                  className='photo'
+                  alt='Las manos de Dios Baños Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-las-manos-de-dios-banos-ecuador_pgbows.jpg'
+                />
+                <div className='card-content'>
+                  <div className='card-content-subtitle'>
+                    <PushpinOutlined /> Actividades
+                  </div>
+                  <div className='card-content-title'>Las Manos de Dios</div>
+                  <Popover className='card-content-popover' placement='topLeft' content={content}>
+                    <span className='reserva-flexible'>
+                      <InfoCircleOutlined /> Reserva Flexible
+                    </span>
+                  </Popover>
                 </div>
-                <div className='card-content-title'>Las Manos de Dios</div>
-                <Popover className='card-content-popover' placement='topLeft' content={content}>
-                  <span className='reserva-flexible'>
-                    <InfoCircleOutlined /> Reserva Flexible
-                  </span>
-                </Popover>
-              </div>
-              <div className='card-footer'>
-                Precio por adulto desde:
-                <div className='card-footer-inside'>
-                  <div className='card-footer-usd'>Usd</div>
-                  <div className='card-footer-number'>10</div>
+                <div className='card-footer'>
+                  Precio por adulto desde:
+                  <div className='card-footer-inside'>
+                    <div className='card-footer-usd'>Usd</div>
+                    <div className='card-footer-number'>10</div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
           <Col span={6}>
-            <div className='card'>
-              <img
-                className='photo'
-                alt='Canyoning Baños Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-canyoning-banos-ecuador_bnmtfl.jpg'
-              />
-              <div className='card-content'>
-                <div className='card-content-subtitle'>
-                  <PushpinOutlined /> Actividades
+            <Card hoverable>
+              <div className='card'>
+                <img
+                  className='photo'
+                  alt='Canyoning Baños Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-canyoning-banos-ecuador_bnmtfl.jpg'
+                />
+                <div className='card-content'>
+                  <div className='card-content-subtitle'>
+                    <PushpinOutlined /> Actividades
+                  </div>
+                  <div className='card-content-title'>Canyoning</div>
+                  <Popover className='card-content-popover' placement='topLeft' content={content}>
+                    <span className='reserva-flexible'>
+                      <InfoCircleOutlined /> Reserva Flexible
+                    </span>
+                  </Popover>
                 </div>
-                <div className='card-content-title'>Canyoning</div>
-                <Popover className='card-content-popover' placement='topLeft' content={content}>
-                  <span className='reserva-flexible'>
-                    <InfoCircleOutlined /> Reserva Flexible
-                  </span>
-                </Popover>
-              </div>
-              <div className='card-footer'>
-                Precio por adulto desde:
-                <div className='card-footer-inside'>
-                  <div className='card-footer-usd'>Usd</div>
-                  <div className='card-footer-number'>30</div>
+                <div className='card-footer'>
+                  Precio por adulto desde:
+                  <div className='card-footer-inside'>
+                    <div className='card-footer-usd'>Usd</div>
+                    <div className='card-footer-number'>30</div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
           <Col span={6}>
-            <div className='card'>
-              <img
-                className='photo'
-                alt='La casa del arbol Baños Ecuador'
-                src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-casa-del-arbol-banos-ecuador_bdb7kv.jpg'
-              />
-              <div className='card-content'>
-                <div className='card-content-subtitle'>
-                  <PushpinOutlined /> Actividades
+            <Card hoverable>
+              <div className='card'>
+                <img
+                  className='photo'
+                  alt='La casa del arbol Baños Ecuador'
+                  src='https://res.cloudinary.com/dlypj8ldu/image/upload/v1634665069/mayotravel/card-casa-del-arbol-banos-ecuador_bdb7kv.jpg'
+                />
+                <div className='card-content'>
+                  <div className='card-content-subtitle'>
+                    <PushpinOutlined /> Actividades
+                  </div>
+                  <div className='card-content-title'>La Casa del Arbol</div>
+                  <Popover className='card-content-popover' placement='topLeft' content={content}>
+                    <span className='reserva-flexible'>
+                      <InfoCircleOutlined /> Reserva Flexible
+                    </span>
+                  </Popover>
                 </div>
-                <div className='card-content-title'>La Casa del Arbol</div>
-                <Popover className='card-content-popover' placement='topLeft' content={content}>
-                  <span className='reserva-flexible'>
-                    <InfoCircleOutlined /> Reserva Flexible
-                  </span>
-                </Popover>
-              </div>
-              <div className='card-footer'>
-                Precio por adulto desde:
-                <div className='card-footer-inside'>
-                  <div className='card-footer-usd'>Usd</div>
-                  <div className='card-footer-number'>8</div>
+                <div className='card-footer'>
+                  Precio por adulto desde:
+                  <div className='card-footer-inside'>
+                    <div className='card-footer-usd'>Usd</div>
+                    <div className='card-footer-number'>8</div>
+                  </div>
                 </div>
               </div>
-            </div>
+            </Card>
           </Col>
         </Row>
       </div>
