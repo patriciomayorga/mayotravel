a41581a2d7cc4f2fc020968c61de332ae59eceb1
Paul Mayorga <jpmayorga@outlook.com>
2021-10-22 12:54:10 -0500

    fix: update logs

--- src/pages/api/carousels.ts
+++ src/pages/api/carousels.ts
@@ -1,5 +1,8 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import { supabase } from '../../lib/supabaseApi';
 
 export default async (_req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' });
+  const { data } = await supabase.from('Carousels').select('*');
+
+  res.status(200).json({ name: 'John Doe', carousels: data });
 };
